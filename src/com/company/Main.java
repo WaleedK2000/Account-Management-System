a9b2a736d8cb163f1868f263a4d5b990612fc41c
WaleedK2000 <74948618+WaleedK2000@users.noreply.github.com>
2021-09-08 20:32:07 +0500

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void print(String val){
+        System.out.println(val);
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        String p1 = "Hello";
+        print(p1);
+    }
+}
